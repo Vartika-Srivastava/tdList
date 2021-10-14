6f1c23dbb867c32010094122ba3d5b6f127f2f5e
Vartika-Srivastava <vartikavarti27@gmail.com>
2021-10-14 22:46:30 +0530

    initial

---
+++ todo_list/src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <p>
+                ReactJs Complete Course: In this complete react tutorial for beginners in Hindi, we will discuss various aspects of React. We will look into why we use react and how React can be used to build amazing Single page applications with a lot of conveniences.
+React is a JavaScript library for creating User Interfaces. React uses JavaScript to deliver amazing user experiences when it comes to serving single-page applications
+</p>
+        </div>
+    )
+}
